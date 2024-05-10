55bd47454ba34a94227bc35661865dc45923084a
vastspark04 <vast_04_sparky@icloud.com>
2024-05-11 06:39:00 +0900

    v1.0.5

---
+++ KAI_KF21_Boramae/CustomAircraftTemplatev2.0-master - KF21/CustomAircraftTemplate/AircraftInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAircraftTemplateKF21
+{
+    public class AircraftInfo
+    {
+
+        //READ ME, IMPORTANT!!!!!!!!
+        //You must change HarmonyId in order for your custom aircraft mod to be compatible with other aircraft mods
+        public const string HarmonyId = "lulul.kf21";
+
+        
+        
+        //The name of the aircraft specified in the External Vehicle Info Component
+       
+        public const string vehicleName = "KAI KF-21";
+       
+        //Names of the prefab name you created and the name of the Assetbundle that you created
+        public const string AircraftAssetbundleName = "kf21";
+        public const string AircraftPrefabName = "KF21";
+
+
+    }
+}
